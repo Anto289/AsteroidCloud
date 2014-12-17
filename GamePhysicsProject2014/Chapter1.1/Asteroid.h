f9ac02a99f71518aced16efa2947ed8933c306a1
Anto289 <P11012024@student.lit.ie>
2014-12-17 21:42:14 +0000

    tempFix

---
+++ GamePhysicsProject2014/Chapter1.1/Asteroid.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICS_OBJECT_H
+#define PHYSICS_OBJECT_H
+
+#include "BulletDynamics/Dynamics/btDynamicsWorld.h"
+
+class Asteroid
+{
+public:
+	Asteroid();
+	~Asteroid();
+private:
+}
+#endif
